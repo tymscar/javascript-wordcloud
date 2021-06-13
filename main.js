6ae7027be1cf30753d307b8cb2afb0dc7e165433
Oscar <tymscar@gmail.com>
2021-06-13 19:43:14 +0100

    Can write to the canvas without overlapping words in a diagonal

--- main.js
+++ main.js
@@ -1,9 +1,71 @@
 import Topic from "./modules/topic.js"
 
 const topicsDiv = document.getElementById("topics__div")
+const canvas = document.getElementById("cloudCanvas")
+const canvasCtx = canvas.getContext('2d');
+
+const gap = 1
+canvas.width = window.innerWidth * 0.7
+canvas.height = canvas.width * 0.5625
 
 
-const getTopicsJson = async (URL)=>{
+canvasCtx.fillStyle = "black"
+canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
+canvasCtx.fillStyle = "white"
+
+
+
+const drawCloud = (topics) => {
+  const placedTopics = []
+
+  topics.forEach(topic => {
+    canvasCtx.font = '20px sans serif'
+    const word = topic.label
+    const currTextSize = canvasCtx.measureText(word)
+
+
+
+
+    let increment = 0
+
+    topic.left = canvas.width / 2 - currTextSize.width / 2
+    topic.width = currTextSize.width
+    topic.down = canvas.height / 2 + currTextSize.actualBoundingBoxAscent / 2 + currTextSize.actualBoundingBoxDescent / 2
+    topic.height = currTextSize.actualBoundingBoxAscent + currTextSize.actualBoundingBoxDescent
+
+
+    let canBePlaced = true
+    let goUp = true
+
+    do {
+      console.log(placedTopics.length)
+      canBePlaced = true
+      if (goUp) {
+        topic.down += increment
+        goUp = false
+      } else {
+        topic.left += increment
+        increment += gap
+        goUp = true
+      }
+
+
+      placedTopics.forEach(placedTopic => {
+        if (topic.intersects(placedTopic)) {
+          canBePlaced = false
+        }
+      })
+
+    } while (!canBePlaced);
+
+
+    canvasCtx.fillText(word, topic.left, topic.down)
+    placedTopics.push(topic)
+  })
+}
+
+
+const getTopicsJson = async (URL) => {
   const topicsResponse = await fetch(URL)
   const topicsJson = await topicsResponse.json()
   const topics = topicsJson["topics"]
@@ -14,13 +76,15 @@ const getTopicsJson = async (URL)=>{
 getTopicsJson("topics.json").then(topicsJson => {
   const topics = []
 
-  topicsJson.forEach(topicJson =>{
-    topics.push(new Topic(topicJson["label"],topicJson["volume"],topicJson["sentimentScore"]))
+  topicsJson.forEach(topicJson => {
+    topics.push(new Topic(topicJson["label"], topicJson["volume"], topicJson["sentimentScore"]))
   })
 
-  topics.forEach(topic=>{
+  topics.forEach(topic => {
     const currentTopicP = document.createElement("p")
     currentTopicP.appendChild(document.createTextNode(`${topic.label} ---> volume: ${topic.volume}, sentiment: ${topic.sentimentScore}`))
     topicsDiv.appendChild(currentTopicP)
   })
+
+  drawCloud(topics)
 })
--- modules/topic.js
+++ modules/topic.js
@@ -1,8 +1,18 @@
 class Topic {
     constructor(label, volume, sentimentScore) {
-      this.label = label;
-      this.volume = volume;
-      this.sentimentScore = sentimentScore;
+        this.label = label
+        this.volume = volume
+        this.sentimentScore = sentimentScore
+
+        this.left = 0
+        this.width = 0
+        this.down = 0
+        this.height = 0
+    }
+    intersects(other) {
+        if (this.left >= other.left + other.width || other.left >= this.left + this.width) return false;
+        if (this.down >= other.down + other.height || other.down >= this.down + this.height) return false;
+        return true
     }
 }
 
